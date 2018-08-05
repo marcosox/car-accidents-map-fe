6914113385b1cccb693b2e2a963ec7b51dcf8538
marcosox <marcosox@users.noreply.github.com>
2018-08-05 19:01:25 +0200

    clean stats page code
    supported by marcosox/car-accidents-map-be@555f02d

--- js/stats.js
+++ js/stats.js
@@ -1,16 +1,17 @@
 /**
  * Ottiene il totale e lo visualizza nel rispettivo campo nominato come la collezione
  */
-function getTotal() {
+function getDBStats() {
 	$(".loading-spinner-enabled").html("<img height=\"24px\" src=\"img/loading_spinner.gif\">");
 	$.ajax({
 		type: 'GET',
-		url: config.backendUrl + "/GetTotal",
+		url: config.backendUrl + "/GetTotals",
 		dataType: 'json',
 		success: function (result) {
 			$("#veicoli").text(result['veicoli']);
 			$("#incidenti").text(result['incidenti']);
 			$("#persone").text(result['persone']);
+			$("#strade").text(result['strade']);
 			const mediaVeicoli = parseFloat(Math.round((result['veicoli'] / result['incidenti']) * 10000) / 10000).toFixed(4);
 			const mediaPersone = parseFloat(Math.round((result['persone'] / result['incidenti']) * 10000) / 10000).toFixed(4);
 			$("#mediaVeicoli").text(mediaVeicoli);
@@ -18,25 +19,9 @@ function getTotal() {
 		},
 		error: function (result) {
 			console.error(result);
-			$("#veicoli").text("Error retrieving data.");
-			$("#incidenti").text("Error retrieving data.");
-			$("#persone").text("Error retrieving data.");
-			$("#mediaVeicoli").text("Error retrieving data.");
-			$("#mediaPersone").text("Error retrieving data.");
-		}
-	});
-	$.ajax({
-		type: 'GET',
-		url: config.backendUrl + "/GetCount?collection=incidenti&field=strada",
-		dataType: 'json',
-		success: function (result) {
-			$("#strade").text(result.length);	// TODO: change this endpoint to one that returns only count
-		},
-		error: function (result) {
-			console.error(result);
-			$("#strade").text("Error retrieving data.");
+			$(".loading-spinner-enabled").text("Error retrieving data.");
 		}
 	});
 }
 
-$(document).ready(getTotal());
+$(document).ready(getDBStats());
